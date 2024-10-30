aa43231fd64266b398dac3678c710f47d4a2ff81
José Delgado <josedelgadosanabria@gmail.com>
2024-10-30 17:50:39 -0300

    Acutalice gitignore para no incluir carpeta media, corregi un error

--- malbum/admin.py
+++ malbum/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Foto
 
 @admin.register(Foto)
-class FotoAdmin(admin.MOdelAdmin):
+class FotoAdmin(admin.ModelAdmin):
   list_display = ('titulo', 'usuario', 'fecha_subida')
   search_fields = ('titulo', 'usuario__username')
   list_filter = ('fecha_subida', 'usuario')
