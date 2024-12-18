d427d1c418b2ffbfd008700a07ebfdd9faa87d2b
José Delgado <josedelgadosanabria@gmail.com>
2024-12-18 17:10:37 -0300

    Orden de las url hizo que no funcione el cambio de contrasena

--- usuario/urls.py
+++ usuario/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('registrar/', registrarUsuario, name="registrarUsuario"),
     path('iniciar-sesion/', auth_views.LoginView.as_view(template_name='usuario/iniciar-sesion.html'), name='iniciarSesion'),
     path('cerrar-sesion/', auth_views.LogoutView.as_view(next_page='inicio'), name='cerrarSesion'),
-    path('<str:username>/', views.perfil_usuario, name='perfil_usuario'),
     path('recuperar-contrasena/', auth_views.PasswordResetView.as_view(template_name='usuario/recuperar-contrasena.html'), name='password_reset'),
     path('recuperar-contrasena/hecho/', auth_views.PasswordResetDoneView.as_view(template_name='usuario/recuperar-contrasena-hecho.html'), name='password_reset_done'),
     path('recuperar-contrasena-confirmar/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='usuario/recuperar-contrasena-confirmar.html'), name='password_reset_confirm'),
@@ -15,4 +14,5 @@ urlpatterns = [
     path("actor/<str:username>/", views.activitypub_actor, name="activitypub_actor"),
     path("inbox/<str:username>/", views.activitypub_inbox, name="activitypub_inbox"),
     path("outbox/<str:username>/", views.activitypub_outbox, name="activitypub_outbox"),
+    path('<str:username>/', views.perfil_usuario, name='perfil_usuario'),
 ]
