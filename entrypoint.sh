7ba5544f2033bf4359e28184c67cd85e9491cd4f
José Delgado <josedelgadosanabria@gmail.com>
2024-12-28 18:33:43 -0300

    no se crea media

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-# Create media directories if they don't exist
-mkdir -p /code/media /code/media/fotos /code/media/profile_pics
-chown -R www-data:www-data /code/media
-chmod -R 775 /code/media
-
-exec "$@"
-
 echo "Applying database migrations..."
 python manage.py migrate --noinput
 
