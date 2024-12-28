03669096af4aad0c3de9877053c1c8b9e1717694
José Delgado <josedelgadosanabria@gmail.com>
2024-12-28 18:26:46 -0300

    Agregando los directorios

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# Create media directories if they don't exist
+mkdir -p /code/media/fotos /code/media/profile_pics
+chown -R www-data:www-data /code/media
+chmod -R 775 /code/media
+
+exec "$@"
+
 echo "Applying database migrations..."
 python manage.py migrate --noinput
 
