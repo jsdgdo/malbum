803b65c8bd605d1c58ddd309609f3b9c7f484a49
José Delgado <josedelgadosanabria@gmail.com>
2024-12-28 18:31:33 -0300

    Agregue el directorio media

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Create media directories if they don't exist
-mkdir -p /code/media/fotos /code/media/profile_pics
+mkdir -p /code/media /code/media/fotos /code/media/profile_pics
 chown -R www-data:www-data /code/media
 chmod -R 775 /code/media
 
