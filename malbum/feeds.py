5390aff04ae73c04bb615017fe421e67469978f8
José Delgado <josedelgadosanabria@gmail.com>
2025-01-21 17:04:06 -0300

    Fix para RSS

--- malbum/feeds.py
+++ malbum/feeds.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.conf import settings
 import os
 from .models import Foto
+from .config import get_valor
 
 class FotoFeed(Feed):
     title = "Malbum - Fotos"
@@ -16,51 +17,54 @@ class FotoFeed(Feed):
         return item.titulo
 
     def item_description(self, item):
-      description = f'<img src="{settings.SITE_URL}{item.imagen.url}" alt="{item.alt_descripcion or "Imagen"}" style="max-width:100%;">'
-      description += f"""
-      <p><strong>Descripción:</strong> {item.descripcion or 'Sin descripción'}</p>
-      <p><strong>Licencia:</strong> {item.licencia or 'Sin licencia especificada'}</p>
-      """
-      if item.advertencia_contenido:
-          description += '<p><strong>Advertencia de contenido:</strong> Esta imagen puede contener contenido sensible.</p>'
+        domain = get_valor('dominio')
+        description = f'<img src="https://{domain}{item.get_medium_url()}" alt="{item.alt_descripcion or "Imagen"}" style="max-width:100%;">'
+        description += f"""
+        <p><strong>Descripción:</strong> {item.descripcion or 'Sin descripción'}</p>
+        <p><strong>Licencia:</strong> {item.licencia or 'Sin licencia especificada'}</p>
+        """
+        if item.advertencia_contenido:
+            description += '<p><strong>Advertencia de contenido:</strong> Esta imagen puede contener contenido sensible.</p>'
 
-      description += '<p><strong>Datos de captura</strong></p>'
-      if item.camara:
-          description += f'<li><strong>Cámara:</strong> {item.camara}</li>'
-      else:
-          description += '<li><strong>Cámara:</strong> No especificada</li>'
-      
-      if item.lente:
-          description += f'<li><strong>Lente:</strong> {item.lente}</li>'
-      else:
-          description += '<li><strong>Lente:</strong> No especificada</li>'
-      
-      if item.configuracion:
-          description += f'<li><strong>Configuración:</strong> {item.configuracion}</li>'
-      else:
-          description += '<li><strong>Configuración:</strong> No especificada</li>'
-      
-      description += '</ul>'
+        description += '<p><strong>Datos de captura</strong></p>'
+        if item.camara:
+            description += f'<li><strong>Cámara:</strong> {item.camara}</li>'
+        else:
+            description += '<li><strong>Cámara:</strong> No especificada</li>'
+        
+        if item.lente:
+            description += f'<li><strong>Lente:</strong> {item.lente}</li>'
+        else:
+            description += '<li><strong>Lente:</strong> No especificada</li>'
+        
+        if item.configuracion:
+            description += f'<li><strong>Configuración:</strong> {item.configuracion}</li>'
+        else:
+            description += '<li><strong>Configuración:</strong> No especificada</li>'
+        
+        description += '</ul>'
 
-      return description
+        return description
 
     def item_link(self, item):
         return reverse('detalle_foto', args=[item.id])  # Assuming you have a detail view for "Foto"
 
     def item_extra_kwargs(self, item):
         # Add additional elements like <image> for RSS
+        domain = get_valor('dominio')
         return {
-            'image': f"{item.imagen.url}"
+            'image': f"https://{domain}{item.get_medium_url()}"
         }
 
     def item_enclosure_url(self, item):
-      return "http://localhost:8080"+item.imagen.url
+        domain = get_valor('dominio')
+        return f"https://{domain}{item.get_medium_url()}"
     
     def item_enclosure_mime_type(self, item):
-      return "image/jpeg"
+        return "image/jpeg"
 
     def item_enclosure_length(self, item):
-      try:
-        return os.path.getsize(item.imagen.path)
-      except FileNotFoundError:
-        return 0  # Fallback if the file doesn't exist
+        try:
+            return os.path.getsize(item.imagen.path)
+        except FileNotFoundError:
+            return 0  # Fallback if the file doesn't exist
